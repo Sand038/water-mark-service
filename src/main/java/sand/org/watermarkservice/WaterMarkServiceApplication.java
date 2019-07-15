54d4067dc485ecbdea383b22c492478caf2a3cf1
Sand038 <sandeepamadhusanka038@gmail.com>
2019-07-15 20:07:29 +0530

    Adding initial commit

---
+++ src/main/java/sand/org/watermarkservice/WaterMarkServiceApplication.java
@@ -0,0 +1,15 @@
+package sand.org.watermarkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterMarkServiceApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(WaterMarkServiceApplication.class, args);
+  }
+
+}
